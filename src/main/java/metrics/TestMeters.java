e2c1c9a5c2dd6b31872f1f92f83e646feb26c387
qinyuan <qinyuan15@sina.com>
2014-06-20 17:49:59 +0800

    update

--- src/main/java/metrics/TestMeters.java
+++ src/main/java/metrics/TestMeters.java
@@ -8,33 +8,30 @@ import java.util.concurrent.TimeUnit;
 
 import static com.codahale.metrics.MetricRegistry.*;
 
+/**
+ * Meteter if used to measures the rate at which a set of event occur
+ * 
+ * @author qinyuan
+ * 
+ */
 public class TestMeters {
-    /**
-     * 实例化一个registry，最核心的一个模块，相当于一个应用程序的metrics系统的容器，维护一个Map
-     */
-    private static final MetricRegistry metrics = new MetricRegistry();
 
-    /**
-     * 在控制台上打印输出
-     */
-    private static ConsoleReporter reporter = ConsoleReporter.forRegistry(
-            metrics).build();
+	private static final MetricRegistry metrics = new MetricRegistry();
+	private static ConsoleReporter reporter = ConsoleReporter.forRegistry(
+			metrics).build();
 
-    /**
-     * 实例化一个Meter
-     */
-    private static final Meter requests = metrics.meter(name(TestMeters.class,
-            "request"));
+	private static final Meter requests = metrics.meter(name(TestMeters.class,
+			"request"));
 
-    public static void handleRequest() {
-        requests.mark();
-    }
+	public static void handleRequest() {
+		requests.mark();
+	}
 
-    public static void main(String[] args) throws InterruptedException {
-        reporter.start(3, TimeUnit.SECONDS);
-        for (int i = 0; i < 20; i++) {
-            handleRequest();
-            Thread.sleep(1000);
-        }
-    }
+	public static void main(String[] args) throws InterruptedException {
+		reporter.start(3, TimeUnit.SECONDS);
+		for (int i = 0; i < 20; i++) {
+			handleRequest();
+			Thread.sleep(1000);
+		}
+	}
 }
---
+++ src/main/java/metrics/TestTimer.java
@@ -0,0 +1,37 @@
+package metrics;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class TestTimer {
+	private static final MetricRegistry metrics = new MetricRegistry();
+	private static ConsoleReporter reporter = ConsoleReporter.forRegistry(
+			metrics).build();
+
+	private static final Timer requests = metrics.timer(MetricRegistry.name(
+			TestTimer.class, "request"));
+
+	public static void handleRequest(int sleep) {
+		Timer.Context context = requests.time();
+		try {
+			Thread.sleep(sleep);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			context.stop();
+		}
+
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		reporter.start(3, TimeUnit.SECONDS);
+		Random random = new Random();
+		for (int i = 0; i < 60; i++) {
+			//handleRequest(random.nextInt(1000));
+			handleRequest(500);
+		}
+	}
+}
