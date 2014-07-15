375d4b1b04b4023e7989fde98686e016ae62134b
qinyuan <qinyuan15@sina.com>
2014-07-15 12:58:16 +0800

    update

---
+++ src/main/java/printf/DecimalFormatExample.java
@@ -0,0 +1,17 @@
+package printf;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by qinyuan on 14-7-15.
+ */
+public class DecimalFormatExample {
+
+    public static void main(String[] args) {
+        DecimalFormat format = new DecimalFormat(",###,###.0000");
+        System.out.println(format.format(111111111.11111111111));
+
+        format = new DecimalFormat(",###,###.0000");
+        System.out.println(format.format(1.11111111111));
+    }
+}
--- src/main/java/printf/PrintExample.java
+++ src/main/java/printf/PrintExample.java
@@ -8,6 +8,7 @@ public class PrintExample {
     public static void main(String[] args) {
         System.out.printf("True is %b\n", true);
         System.out.printf("False is %b\n", false);
-        System.out.printf("This is double %.2f", Math.PI * 100000);
+        System.out.printf("This is double %.2f\n", Math.PI * 100000);
+        System.out.printf("This is double %f\n", 2.11);
     }
 }
